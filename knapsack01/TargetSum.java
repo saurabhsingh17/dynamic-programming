8de9c4b2bf1781145beac78cf701d52fc09f6b8a
saurabhsingh <saurabh623a@gmail.com>
2024-01-18 22:04:39 +0530

    Day-5

--- knapsack01/TargetSum.java
+++ knapsack01/TargetSum.java
@@ -1,5 +1,33 @@
 package knapsack01;
 
+import java.util.Arrays;
+
 public class TargetSum {
     //exactly same as count subset with given difference
-}
+    //one more optimised solution
+        public static int findTargetSumWays(int[] nums, int S) {
+            int total = Arrays.stream(nums).sum();
+            int[] dp = new int[2 * total + 1];
+            dp[nums[0] + total] = 1;
+            dp[-nums[0] + total] += 1;
+
+            for (int i = 1; i < nums.length; i++) {
+                int[] next = new int[2 * total + 1];
+                for (int sum = -total; sum <= total; sum++) {
+                    if (dp[sum + total] > 0) {
+                        next[sum + nums[i] + total] += dp[sum + total];
+                        next[sum - nums[i] + total] += dp[sum + total];
+                    }
+                }
+                dp = next;
+            }
+
+            return Math.abs(S) > total ? 0 : dp[S + total];
+        }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,1,1};
+        int diff = 3;
+        System.out.println(findTargetSumWays(arr,3));
+    }
+    }
