2135f1bdddb16cce221fadbb03967eca61888d03
saurabhsingh <saurabh623a@gmail.com>
2024-01-19 22:53:38 +0530

    Day-6

--- unboundedknapsack/CoinChangeMinimumCoins.java
+++ unboundedknapsack/CoinChangeMinimumCoins.java
@@ -9,7 +9,7 @@ public class CoinChangeMinimumCoins {
 
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int dp[][] =new int[n+1][amount+1];
+        int[][] dp =new int[n+1][amount+1];
         //Arrays.stream(dp).forEach(row ->Arrays.fill(row,-1));
         for(int i = 0;i<amount+1;i++){
             dp[0][i] = Integer.MAX_VALUE-1;
